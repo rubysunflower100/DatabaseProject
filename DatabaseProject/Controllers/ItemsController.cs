c9e79cb78444f3efa98c9d104272cbd3d63ad84d
rubysunflower100 <159494200+rubysunflower100@users.noreply.github.com>
2024-11-11 21:55:17 -0600

    Add files via upload

---
+++ DatabaseProject/Controllers/ItemsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatabaseProject.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
