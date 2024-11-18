2d297c1dbe8794177e6db334756f65107ae0c0f7
rubysunflower100 <159494200+rubysunflower100@users.noreply.github.com>
2024-11-17 22:55:25 -0600

    Add files via upload

--- DatabaseProject/Controllers/ItemsController.cs
+++ DatabaseProject/Controllers/ItemsController.cs
@@ -15,8 +15,55 @@ namespace DatabaseProject.Controllers
         public async Task<IActionResult> Index()
         {
             var item = await _context.Items.ToListAsync();
+            return View(item);
+        }
+        public IActionResult Create()
+        {
             return View();
         }
-    }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id","Name","Price")] Item item)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Items.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("index");
+            }
+            return View(item);
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
+            return View(item);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Price")] Item item)
+        {
+            if (ModelState.IsValid) {
+                _context.Update(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+		public async Task<IActionResult> Delete(int id)
+		{
+			var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
+			return View(item);
+		}
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            if(item != null)
+            {
+                _context.Items.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+	}
+
 
 }
